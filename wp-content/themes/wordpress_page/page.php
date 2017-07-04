bb63fe90e6e25164cb6ddd4a9f80a33d5977c4af
mokon1 <michalina.okon@gmail.com>
2017-07-04 20:43:14 +0200

    added page.php

---
+++ wp-content/themes/wordpress_page/page.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+
+<section id="main-section">
+    <main class="main-content">
+        <div class="wraper">
+            <p class="main-text">
+               <?php
+                while ( have_posts() ) : the_post();			
+                    the_content();
+                endwhile; 
+                ?>
+            </p>
+        </div>
+    </main>
+     <?php get_sidebar(); ?>
+</section>
+
+<?php get_footer(); ?>
